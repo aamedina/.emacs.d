69bb6e33ad5282a10e131c0c896f2f72c4c11dea
Adrian Medina <adrian.medina@mail.yu.edu>
2014-01-30 16:30:35 -0500

    use custom.el file

--- init.el
+++ init.el
@@ -3,17 +3,13 @@
 ;; inspired by Technomancy's emacs dotfiles
 ;; https://github.com/technomancy/dotfiles/blob/master/.emacs.d
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("9370aeac615012366188359cb05011aea721c73e1cb194798bc18576025cabeb" default))))
-
 (require 'cl)
 
 (setq ido-use-virtual-buffers t
-      inhibit-startup-message t)
+      inhibit-startup-message t
+      custom-file (expand-file-name "~/.emacs.d/custom.el"))
+
+(load custom-file t)
 
 (require 'package)
 (add-to-list 'package-archives
